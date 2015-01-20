65f4046c8346dc5a8b1dc6801a9dd08cfa842409
David Fisher <tibbon@gmail.com>
2015-01-20 15:35:39 -0500

    Its alive jim

---
+++ lib/conway.rb
@@ -0,0 +1,66 @@
+class Cell
+
+  include Comparable
+
+  def initialize(status: :dead)
+    @status = status
+  end
+
+  def status
+    @status
+  end
+
+  def survive(alive_neighbors:)
+    if status == :alive
+      if alive_neighbors < 2
+        new_dead_cell
+      elsif alive_neighbors > 3
+        new_dead_cell
+      else
+        new_alive_cell
+      end
+    else
+      if alive_neighbors == 3
+        new_alive_cell
+      else
+        new_dead_cell
+      end
+    end
+  end
+
+  def <=>(other)
+    status <=> other.status
+  end
+
+  private
+  def new_alive_cell
+    Cell.new(status: :alive)
+  end
+
+  def new_dead_cell
+    Cell.new()
+  end
+end
+
+class World
+  def initialize(dimensions: 64, cells: [])
+    if cells.length < 1
+      @cells = Array.new(dimensions) {
+        Array.new(dimensions) { Cell.new }
+      }
+    else
+      @cells = cells
+    end
+  end
+
+  def cells
+    @cells
+  end
+
+  def next_world
+    # for each cell, count alive neighbors
+    World.new()
+  end
+
+  private
+end
--- spec/conway_spec.rb
+++ spec/conway_spec.rb
@@ -1,1 +1,96 @@
 require_relative '../lib/conway'
+
+RSpec.describe Cell do
+  describe '#initialize' do
+    context 'optionally takes a status argument to initialize it, defaults dead' do
+      it 'is initialized alive' do
+        cell = Cell.new(status: :alive)
+        expect(cell).to be_a Cell
+        expect(cell.status).to be :alive
+      end
+
+       it 'is initialized dead' do
+        cell = Cell.new()
+        expect(cell).to be_a Cell
+        expect(cell.status).to be :dead
+      end
+    end
+  end
+
+  describe '#survive' do
+    subject(:alive_cell) { Cell.new(status: :alive) }
+    subject(:dead_cell) { Cell.new }
+    context 'alive cell with fewer than two alive neighbors' do
+      it 'will return a new dead cell' do
+        expect(alive_cell.survive(alive_neighbors: 0)).to eq dead_cell
+        expect(alive_cell.survive(alive_neighbors: 1)).to eq dead_cell
+      end
+    end
+
+    context 'alive cell with more than 3 live neighbors' do
+      it 'will return a new dead cell' do
+        expect(alive_cell.survive(alive_neighbors: 4)).to eq dead_cell
+      end
+    end
+
+    context 'alive cell with 2 or 3 live neighbors' do
+      it 'will return a new alive cell' do
+        expect(alive_cell.survive(alive_neighbors: 2)).to eq alive_cell
+        expect(alive_cell.survive(alive_neighbors: 3)).to eq alive_cell
+      end
+    end
+
+    context 'dead cell without 3 live neighbors' do
+      it 'stays dead' do
+        expect(dead_cell.survive(alive_neighbors: 2)).to eq dead_cell
+        expect(dead_cell.survive(alive_neighbors: 4)).to eq dead_cell
+      end
+    end
+
+    context 'dead cell with 3 live neighbors' do
+      it 'will return a new alive cell' do
+        expect(dead_cell.survive(alive_neighbors: 3)).to eq alive_cell
+      end
+    end
+  end
+end
+
+RSpec.describe World do
+
+  describe '#initialize' do
+    subject(:world) { World.new(dimensions: 20) }
+    subject(:seed_cells) { [
+                            [Cell.new(status: :alive),Cell.new(status: :alive),Cell.new(status: :dead)],
+                            [Cell.new(status: :alive),Cell.new(status: :dead),Cell.new(status: :alive)],
+                            [Cell.new(status: :dead),Cell.new(status: :dead),Cell.new(status: :dead)]] }
+    it 'creates a world' do
+      expect(world).to be_a World
+    end
+    it 'generates a 2d grid' do
+      expect(world.cells).to be_an Array
+      expect(world.cells.first).to be_an Array
+    end
+    it 'optionally takes dimensions, defaults to 64x64 for dimensions' do
+      expect(World.new).to be_a World
+    end
+    it 'optionally takes an array of a new world' do
+      expect(World.new(cells: seed_cells)).to be_a World
+    end
+  end
+
+  describe '#next_world' do
+    subject(:seed_cells) { [
+                            [Cell.new(status: :alive),Cell.new(status: :alive),Cell.new(status: :dead)],
+                            [Cell.new(status: :alive),Cell.new(status: :dead),Cell.new(status: :alive)],
+                            [Cell.new(status: :dead),Cell.new(status: :dead),Cell.new(status: :dead)]] }
+    subject(:result_cells) { [
+                            [Cell.new(status: :alive),Cell.new(status: :alive),Cell.new(status: :dead)],
+                            [Cell.new(status: :alive),Cell.new(status: :dead),Cell.new(status: :dead)],
+                            [Cell.new(status: :dead),Cell.new(status: :dead),Cell.new(status: :dead)]] }
+    subject(:world) { World.new(cells: seed_cells) }
+    it 'generates the next world' do
+      expect(world.next_world).to be_a World
+      expect(world.next_world.cells).to eq result_cells
+    end
+  end
+end
